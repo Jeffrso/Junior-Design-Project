5d1c6826f14df3d2ec29ba5d4e727b053e0d7b91
KatieEagan <142842535+KatieEagan@users.noreply.github.com>
2026-09-24 20:48:36 -0400

    LedOn function added to prove what state we are in. No code in function yet.
    Added a function that will turn on leds, and started adding for loop to determine which state we are in, which will correspond to the lit led. Function declarations in states as well.

--- Junior_Design_Project/src/state_machine.cpp
+++ Junior_Design_Project/src/state_machine.cpp
@@ -18,6 +18,7 @@ RobotState currentState = STATE_IDLE;
 //function stuff
 bool externalSignal();
 void stateMachineUpdate();
+void ledOn(int number);
 
 // NEED SOME INITIALIZATION STUFF HERE
 
@@ -25,9 +26,35 @@ bool externalSignal(){
 
 }
 
+void ledOn(int number){
+
+    if(number == 1) {
+
+    } else if(number == 2) {
+
+    } else if(number == 3) {
+
+    } else if(number == 4) {
+
+    } else if(number == 5) {
+
+    } else if(number == 6) {
+
+    } else if(number == 7) {
+
+    } else {
+        // base case, maybe just have an on LED
+    }
+
+}
+
 void stateMachineUpdate() {
+    int number = 0;
+
     switch (currentState) {
         case STATE_IDLE:
+        ledOn(1);
+
             if(externalSignal()) {
                 Serial.print("idle to drive wall turn");
                 currentState = STATE_DRIVE_WALL_TURN;
@@ -36,6 +63,8 @@ void stateMachineUpdate() {
 
             break;
         case STATE_INITIALIZE:
+        ledOn(2);
+
             if(externalSignal()) {
                 Serial.print("init");
                 currentState = STATE_IDLE;
@@ -43,24 +72,32 @@ void stateMachineUpdate() {
             break;
         // COULD REMOVE THEDRIVE STATE AND JUST HAVE RETURN HOME?????????
         case STATE_DRIVE:
+        ledOn(3);
+
             if(externalSignal()) {
                 Serial.print("drive to");
-                currentState = STATE_DRIVE_WALL_TURN;
+                currentState = STATE_RETURN_HOME;
             }
             break;
         case STATE_FIND_LANE:
+        ledOn(4);
+
             if(externalSignal()) {
                 Serial.print("find lane to follow lane");
                 currentState = STATE_LANE_FOLLOW;
             }
             break;
         case STATE_LANE_FOLLOW:
+        ledOn(5);
+
             if(externalSignal()) {
                 Serial.print("lane follow to turn");
                 currentState = STATE_TURN;
             }
             break;
         case STATE_DRIVE_WALL_TURN:
+        ledOn(6);
+
             if(externalSignal()) {
                 Serial.print("drive wall turn to find lane");
                 currentState = STATE_FIND_LANE;
@@ -68,6 +105,8 @@ void stateMachineUpdate() {
 
             break;
         case STATE_TURN:
+        ledOn(7);
+
             if(externalSignal()) {
                 Serial.print("turn to drive");
                 currentState = STATE_DRIVE;
@@ -75,6 +114,8 @@ void stateMachineUpdate() {
 
             break;
         case STATE_RETURN_HOME:
+        ledOn(8);
+
             if(externalSignal()) {
                 Serial.print("return home to idle");
                 currentState = STATE_IDLE;
